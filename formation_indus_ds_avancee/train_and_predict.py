83478f29a1198a0c8b5efcb11e17d71cadb2c71b
trikiamine23 <amine.triki@ca-pacifica.fr>
2024-12-10 15:42:20 +0000

    model registry

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -17,8 +17,8 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     target = 'Ba_avg'
     X = features.drop(columns=[target])
     y = features[target]
-    with mlflow.start_run():
-        # insert autolog here ...
+    with mlflow.start_run(run_name="Test_Amine"):
+        mlflow.sklearn.autolog()
         model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
         model.fit(X, y)
     time_str = time.strftime('%Y%m%d-%H%M%S')
